f6f9fedd809345c9b6a317741a2c7177c64db0c9
zainab-frfr <135985226+zainab-frfr@users.noreply.github.com>
2023-11-24 16:04:14 +0500

    zainab frfr

--- src/budgetbobby/BudgetBobby.java
+++ src/budgetbobby/BudgetBobby.java
@@ -14,13 +14,7 @@ public class BudgetBobby {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a = 10;
-        int b = 79;
-        System.out.println(b+a);
-        System.out.println("yayy we did it");
-        for (int i = 0; i < ; i++) {
-            System.out.println("WUHOOOOOOOOO")
-        }
+        //main
     }
     
 }
